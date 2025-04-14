8799504a6ced7e034da1747326433bef3e33f3f5
sime <simeleo20@gmail.com>
2025-04-14 11:41:05 +0200

    continuo database

--- pages/models.py
+++ pages/models.py
@@ -18,3 +18,26 @@ class Item(models.Model):
         ],
         default="PERSONAL"
     )
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True, blank=True)  # ForeignKey to Image model
+    event = models.ForeignKey('Event', on_delete=models.CASCADE, null=True, blank=True)  # ForeignKey to Event model
+
+class Restoration(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField(default="2000-01-01")  # Add parentheses to properly define the field
+    type = models.CharField(  # Use CharField with choices for an enum-like behavior
+        max_length=20,
+        choices=[
+            ('RESTORATION', 'restoration'),
+            ('REPAIR', 'repair'),
+            ('CLEANING', 'cleaning'),
+        ],
+        default="RESTORATION"
+    )
+    description = models.CharField(max_length=1000)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+    year = models.IntegerField()
+    location = models.CharField(max_length=100)
+    description = models.CharField(max_length=2500)
