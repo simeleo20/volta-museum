9c20bd963f4d03d72da818a2d90094dbd9ec8fd2
simonkonom <simo.konom@gmail.com>
2025-05-05 12:55:43 +0200

    sistemato il ModuleNotFound

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Item, Event, Restoration, Image
 
 def home(request):
-    template = loader.get_template("home.html")
-    return HttpResponse(template.render())
+    return render(request, 'home.html')
+
+def pianifica_visita(request):
+    return render(request, 'visita/pianifica.html')
+
+def orari(request):
+    return render(request, 'visita/orari.html')
+
+def opere(request):
+    return render(request, 'esplora/opere.html')
+
+def vita_volta(request):
+    return render(request, 'esplora/vita.html')
+
+def gioca(request):
+    return render(request, 'gioca.html')
+
+def search(request):
+    query = request.GET.get('q', '')
+    # Implementa la logica di ricerca qui
+    return render(request, 'search.html', {'query': query, 'results': []})
 
 def item(request, item_name):
     template = loader.get_template("item.html")
